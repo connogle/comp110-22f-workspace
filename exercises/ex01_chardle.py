85bcb15619ef467770d56ded08f75a8a0c1f791d
Connor Lee <connogle@ad.unc.edu>
2022-08-25 11:07:58 -0400

    Made Format Prettier

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -6,13 +6,16 @@ user_word: str = input('Enter a 5-character word: ')
 if len(user_word) != 5:
     print('Error: Word must contain 5 characters')
     exit()
+
 user_character: str = input('Enter a single character: ')
 if len(user_character) != 1:
     print('Error: Character must be a single character.')
     exit()
+print('Searching for ' + user_character + ' in ' + user_word)
+
 character_instances: int = 0
 index_number: int = 0
-print('Searching for ' + user_character + ' in ' + user_word)
+
 if user_character == user_word[index_number]:
     character_instances += 1
     print(user_character + ' found at index ' + str(index_number))
