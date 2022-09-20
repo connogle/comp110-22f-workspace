2bbd0a69a436f70fab741305bc596fe56864770d
Connor Lee <connogle@ad.unc.edu>
2022-09-20 16:52:31 -0400

    class activities

---
+++ lessons/imports.py
@@ -0,0 +1,14 @@
+"""Examples of importing in python."""
+
+
+from lessons import helpers
+
+
+def main() -> None:
+    """Entry point of program."""
+    print(helpers.powerful(2, 4))
+    print(helpers.THE_ANSWER)
+
+
+if __name__ == '__main__':
+    main()
---
+++ sandbox/sorting.py
@@ -0,0 +1,8 @@
+"""I am bored so I will sort."""
+
+from array import *
+
+arr = array('i', [1, 2, 3, 4, 5])
+
+print(arr)
+
