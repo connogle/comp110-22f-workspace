173adb2bebef4b157ba08393efd2dfb24355cf41
Connor Lee <connogle@ad.unc.edu>
2022-10-20 16:53:10 -0400

    EX08

--- exercises/ex08/data_utils.py
+++ exercises/ex08/data_utils.py
@@ -20,8 +20,7 @@ def read_csv_rows(filename: str) -> list[dict[str, str]]:
     return result
 
 
-
-def coulumn_values(table: list[dict[str, str]], column: 'str') -> list[str]:
+def column_values(table: list[dict[str, str]], column: 'str') -> list[str]:
     """Produce a list[str] of hte values in a particular column."""
     result: list[str] = []
     for row in table:
@@ -30,11 +29,54 @@ def coulumn_values(table: list[dict[str, str]], column: 'str') -> list[str]:
     return result
 
 
-
 def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
     """Transform a row-oriented table to a column-oriented table."""
     result: dict[str, list[str]] = {}
     first_row: dict[str, str] = row_table[0]
     for column in first_row:
-        result[column] = coulumn_values(row_table, column)
+        result[column] = column_values(row_table, column)
     return result
+
+
+def head(column_table: dict[str, list[str]], rows: int) -> dict[str, list[str]]:
+    """Produces a new column-based table with only N number of rows."""
+    result: dict[str, list[str]] = {}
+    for column in column_table:
+        rows_in_column: list[str] = []
+        for i in range(0, rows):
+            rows_in_column.append(column_table[column][i])
+        result[column] = rows_in_column
+    return result
+
+
+def select(column_table: dict[str, list[str]], column_names: list[str]) -> dict[str, list[str]]:
+    """Produces a new column-based table with only specificly selected columns."""
+    result: dict[str, list[str]] = {}
+    for column in column_names:
+        result[column] = column_table[column]
+    return result
+
+
+def concat(table_1: dict[str, list[str]], table_2: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Takes two column-based tables and combines them into one."""
+    result: dict[str, list[str]] = {}
+    for column in table_1:
+        result[column] = table_1[column]
+    for column in table_2:
+        if column in result:
+            for i in range(0, len(table_2[column])):
+                result[column].append(table_2[column][i])
+        else:
+            result[column] = table_2[column]
+    return result
+
+
+def count(values: list[str]) -> dict[str, int]:
+    """Given a list of strings, returns a dict with the frequency of each string value."""
+    val_frequency: dict[str, int] = {}
+    for value in values:
+        if value in val_frequency:
+            val_frequency[value] += 1
+        else:
+            val_frequency[value] = 1
+    return val_frequency
